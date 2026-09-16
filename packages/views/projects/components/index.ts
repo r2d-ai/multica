04867181673d033051b44d5e42b13fbf29c4d59d
Huy Doan <tui@pm.me>
2026-09-16 11:11:01 +0700

    feat(r2d): route project detail through capability gate

--- packages/views/projects/components/index.ts
+++ packages/views/projects/components/index.ts
@@ -1,5 +1,5 @@
 export { ProjectsPage } from "./projects-page";
-export { R2DProjectDetail as ProjectDetail } from "./r2d-project-sharing";
+export { R2DProjectDetail as ProjectDetail } from "./r2d-project-detail";
 export { ProjectPicker } from "./project-picker";
 export { ProjectChip } from "./project-chip";
 export { ProjectMentionCard } from "./project-mention-card";
